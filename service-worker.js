0e33223163eb7980fd0baff8c91e22f07d419945
xdxxxx <olegtomashivski@gmail.com>
2025-12-31 21:44:03 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -40,14 +40,5 @@ self.addEventListener('fetch', event => {
   );
 });
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(assets))
-  );
-});
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
-  );
-});
+
