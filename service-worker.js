463f10a6dbc3709452214e6bba8637d943078d7f
xdxxxx <olegtomashivski@gmail.com>
2025-12-31 21:01:01 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'smokehub-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/payment.html',
+  '/kwiz.html',
+  '/reviews.html',
+  '/style.css',
+  '/images/apple-mixed.jpg',
+  '/images/bali-triple-shot.jpg',
+  '/images/blackcurrant-grape.jpg',
+  '/images/blik-logo.png',
+  '/images/blueberry-currant.jpg',
+  '/images/cola-lemon.jpg',
+  '/images/cranberry-mors.jpg',
+  '/images/Double-grape.jpg',
+  '/images/ice-shot.jpg',
+  '/images/logo.png',
+  '/images/monobank-logo.png',
+  '/images/multifruit.jpg',
+  '/images/pineapple-lemonade.jpg',
+  '/images/RedBull-blueraspberry.jpg',
+  '/images/spermint.jpg',
+  '/images/watermalon-melon.jpg',
+  '/images/xros-catridge-2ml.jpg',
+  '/images/xros-catridge-3ml.jpg',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
