3c108f9e34249f0b1a5cb2c122fc2137086d1278
xdxxxx <olegtomashivski@gmail.com>
2026-01-12 01:08:23 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,5 @@
 const CACHE_NAME = 'smokehub-cache-v1';
-const urlsToCache = [
-  '/',
+const urlsToCache = [ '/',
   '/index.html',
   '/payment.html',
   '/kwiz.html',
@@ -23,13 +22,25 @@ const urlsToCache = [
   '/images/spermint.jpg',
   '/images/watermalon-melon.jpg',
   '/images/xros-catridge-2ml.jpg',
-  '/images/xros-catridge-3ml.jpg',
-];
+  '/images/xros-catridge-3ml.jpg', ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting()) // одразу активує SW
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    ).then(() => self.clients.claim())
   );
 });
 
@@ -37,6 +48,7 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
+      .catch(() => caches.match('/')) // fallback на index.html
   );
 });
 
